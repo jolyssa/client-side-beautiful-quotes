602c8bebc793620c8fdb6380f10a01d2a8f2e291
jolyssa <jolyssa.armstrong@gmail.com>
2024-12-01 02:30:37 -0500

    added some styling and one function to hide the div

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //server side js
 
-document.querySelector('button').addEventListener('click', getQuote)
+document.querySelector('.button').addEventListener('click', getQuote)
 
-function getQuote(){
+function getQuote() {
 
     let param = document.querySelector('input').value
     let url = `https://beautiful-quotes-api.onrender.com/api/${param}` //put actual link here
@@ -10,12 +10,18 @@ function getQuote(){
     fetch(url)
         .then(res => res.json()) //parse response as JSON
         .then(data => {
-             console.log(data)
-            //  let quote = data[beautifulQuotes.quotes]
+            console.log(data)
+            
             document.querySelector('#name').innerText = data.name
             document.querySelector('#type').innerText = data.type
-             document.querySelector('#quote').innerText = data.quote
-        }
-    )
+            document.querySelector('#quote').innerText = data.quote
+        })
 
+}
+
+document.querySelector('.button').addEventListener('click', displayQuote)
+
+function displayQuote(){
+    let quote = document.querySelector('.showQuote')
+	quote.classList.toggle('hidden')
 }
